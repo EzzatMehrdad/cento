612a63ba461e203d359261ef2ea19c92eb41447e
Ezzat Mehrdad <72700134+EzzatMehrdad@users.noreply.github.com>
2022-09-01 11:11:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const butHom = document.querySelector('.home-button');
+const butTim = document.querySelector('.times-button');
+const navBar = document.getElementById('nav-bar');
+butHom.addEventListener('click', function(){
+    if (butHom.className !==""){
+        butHom.style.display="none";
+        butTim.style.display= "block";
+        navBar.classList.add("show-nav");
+    }
+})
+butTim.addEventListener('click', function(){
+    if(butHom.className !==""){
+        this.style.display="none";
+        butHom.style.display="block";
+        navBar.classList.remove("show-nav");
+    }
+})
+
